1546e3b8635371162fc49c5966145a3b26ff7658
Zühre Koçhisar <zuhrekochisar@gmail.com>
2021-01-30 22:59:26 +0300

    Proje dosyası ekle.

---
+++ Entities/Concrete/Product.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+   public class Product : IEntity  //Public = Bu class a diğer katmanlar da ulaşabilsin demek. O yüzden sonradan ekledik.
+{
+        public int ProductId { get; set; }
+        public int CategoryId { get; set; }
+        public string ProductName { get; set; }
+        public short UnitInStock { get; set; } //int in bir küçüğü.
+        public decimal UnitPrice { get; set; }
+
+    }
+}
