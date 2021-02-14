c260ebc99c46fad3d570c4ca3054aabf5f42a0bb
Zühre Koçhisar <zuhrekochisar@gmail.com>
2021-02-14 23:01:26 +0300

    11.Gün

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]  //Attribute -- Bir class ile ilgili bilgi verme, onu imzalama.
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]  
+        public List<Product> Get()
+        {
+            return new List<Product>
+            {
+                new Product{ProductId = 1, ProductName = "Elma"},
+                new Product{ProductId = 2, ProductName = "Armut"}
+            };
+        }
+    }
+}
